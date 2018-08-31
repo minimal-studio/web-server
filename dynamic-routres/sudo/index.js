e76fb7464260723dd68ed19c313eff6623a4ef19
SANGET <AlexZhang@Alexs-iMac.local>
2018-08-31 20:37:50 +0800

    完善一部分功能

--- dynamic-routres/sudo/index.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let ejs = require('ejs');
-let path = require('path');
-
-let router = async (req, res) => {
-  res.send('ok');
-}
-
-module.exports = router;
--- routers/notfound.js
+++
@@ -1,5 +0,0 @@
-let notFound = (req, res) => {
-  res.send('not found');
-}
-
-module.exports = notFound;
