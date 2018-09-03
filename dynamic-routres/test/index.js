d8752c3e207721b693ea84b1e9f5a8b2dad0f690
sanget <zxxxj@zhangxiangjiedeMacBook-Pro.local>
2018-09-03 19:03:53 +0800

    updatep

--- dynamic-routres/test/index.js
+++ dynamic-routres/test/index.js
@@ -6,11 +6,8 @@ let testData = require('./test-data');
 
 let testRouter = async (req, res) => {
   res.json({
-    Header: {
-      Code: 0,
-      Desc: '成功'
-    },
-    Data: testData
+    err: null,
+    data: testData
   });
 }
 
