f6913f15a23fce76b21864399cbaeaba002b344b
SANGET <AlexZhang@Alexs-iMac.local>
2019-10-01 11:48:22 +0800

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
+import "reflect-metadata";
+import { createConnection, Connection } from "typeorm";
+
 import errorHandler from "errorhandler";
 
 import app from "./app";
 import { Port } from "./configs/site";
 
-/**
- * Error Handler. Provides full stack - remove for production
- */
-app.use(errorHandler());
+createConnection().then((connection: Connection) => {
+  /**
+   * Error Handler. Provides full stack - remove for production
+   */
+  app.use(errorHandler());
 
-/**
- * Start Express server.
- */
-const server = app.listen(Port, () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    Port,
-    app.get("env")
-  );
-  console.log("  Press CTRL-C to stop\n");
+  /**
+   * Start Express server.
+   */
+  app.listen(Port, () => {
+    console.log(
+      "  App is running at http://localhost:%d in %s mode",
+      Port,
+      app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
+  });
 });
-
-export default server;
