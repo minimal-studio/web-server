4602b8ee3b3dfac5d38a5a2615380389a6edc8f1
SANGET <AlexZhang@Alexs-iMac.local>
2019-10-02 10:40:03 +0800

    fix: remove files

--- sql/create-user.sql
+++
@@ -1,9 +0,0 @@
--- Create user for database
-CREATE USER 'test'@'localhost' IDENTIFIED BY 'test';
-GRANT ALL PRIVILEGES ON * . * TO 'test'@'localhost';
-
--- Create database
-CREATE DATABASE test;
-
--- Fix the node mysql bug
-ALTER USER 'test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'test';
